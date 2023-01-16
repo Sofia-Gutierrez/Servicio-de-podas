8e3ccf15a0f5bdca404cda6adc3469b74b108c1f
Sofia_Gutierrez <sofiagutierrez1845@gmail.com>
2023-01-16 15:10:28 -0300

    animando el btn de la navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,14 +4,25 @@ import { Link } from "react-scroll";
 
 const Navbar = () => {
 
+    const handleClickBtn = () => {
+        const btn = document.getElementById("nav-btn");
+        const navContent = document.getElementById("nav-content");
+
+        btn.classList.toggle("open");
+        navContent.classList.toggle("open");
+    }
+
     useAos();
 
         return(
-            <nav className="nav" data-aos="fade-down">  
-                <input type="checkbox" id="toggler" />
-                <label for="toggler"><i className="fa-solid fa-bars"></i></label>
-                <div className="nav-content">
-                    <div className="nav-logo">LOGO</div>
+            <nav className="nav" data-aos="fade-down"> 
+                <div id="nav-btn" onClick={handleClickBtn}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <div className="nav-content" id="nav-content">
+                <div className="nav-logo">LOGO</div>
                     <ul className="nav-links">
                         <li className="nav-li active"><Link className="link" to="home" spy={true} smooth={true} offset={-50} duration={500}>Inicio</Link></li>
                         <li className="nav-li"><Link className="link" to="services" spy={true} smooth={true} offset={-50} duration={500}>Servicios</Link></li>
